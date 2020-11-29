2e9278d2b35fbb3b097ab3d4baa12854c4b35c75
yuu383 <yuukikokubo.pg@gmail.com>
2020-11-29 22:55:11 +0900

    dynamic imports

---
+++ packages/dynamic-imports/webpack.config.js
@@ -0,0 +1,11 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "[name].bundle.js",
+    chunkFilename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+};
